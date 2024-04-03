9e3470a416aef9f9f0b6474f4280af9e1c621472
victorcolonfigueroa <victor.colonfigueroa@gmail.com>
2024-04-02 21:55:55 -0400

    End of day

--- binary_trees.h
+++ binary_trees.h
@@ -25,5 +25,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 void binary_tree_delete(binary_tree_t *tree);
 int binary_tree_is_leaf(const binary_tree_t *node);
+int binary_tree_is_root(const binary_tree_t *node);
+size_t binary_tree_size(const binary_tree_t *tree);
+size_t binary_tree_leaves(const binary_tree_t *tree);
+size_t binary_tree_nodes(const binary_tree_t *tree);
+int binary_tree_balance(const binary_tree_t *tree);
 
 #endif
